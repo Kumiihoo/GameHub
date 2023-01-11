583cc48abd25d2f2682a42b012b457dae86e4941
Mario Medina <maralfonso.medina@gmail.com>
2023-01-11 20:49:36 +0100

    Initial commit

---
+++ app/src/main/java/com/mariomedina/gamehub/activity/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.mariomedina.gamehub.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
+import com.mariomedina.gamehub.MainActivity
+import com.mariomedina.gamehub.R
+import com.mariomedina.gamehub.auth.LoginActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            if (user == null)
+                startActivity(Intent(this, LoginActivity::class.java))
+            else
+                startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/mariomedina/gamehub/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mariomedina.gamehub.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mariomedina.gamehub.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
