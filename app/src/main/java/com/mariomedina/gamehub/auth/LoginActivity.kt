180a170f9d78c44eb70e51270cab6b429383d4b4
Mario Medina <kumihontou@gmail.com>
2023-01-12 17:15:07 +0100

    Vista de Registro hecha

--- app/src/main/java/com/mariomedina/gamehub/auth/LoginActivity.kt
+++ app/src/main/java/com/mariomedina/gamehub/auth/LoginActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.mariomedina.gamehub.MainActivity
+import com.mariomedina.gamehub.R
 import com.mariomedina.gamehub.databinding.ActivityLoginBinding
 import java.util.concurrent.TimeUnit
 
@@ -21,12 +27,18 @@ class LoginActivity : AppCompatActivity() {
     val auth = FirebaseAuth.getInstance()
     private var verificationId: String? = null
 
+    private lateinit var dialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dialog = AlertDialog.Builder(this).setView(R.layout.loading_layout)
+            .setCancelable(false)
+            .create()
 
         binding.sendOTP.setOnClickListener{
             if (binding.userNumber.text!!.isEmpty()) {
@@ -49,6 +61,8 @@ class LoginActivity : AppCompatActivity() {
 
         //binding.sendOTP.showLoadingButton()
 
+        dialog.show()
+
         val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
 
         signInWithPhoneAuthCredential(credential)
@@ -59,11 +73,14 @@ class LoginActivity : AppCompatActivity() {
 
         //binding.sendOTP.showLoadingButton()
 
+        dialog.show()
+
         val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
 
                 //binding.sendOTP.showNormalButton()
+
                 signInWithPhoneAuthCredential(credential)
             }
 
@@ -77,6 +94,7 @@ class LoginActivity : AppCompatActivity() {
             ) {
                 this@LoginActivity.verificationId = verificationId
 
+                dialog.dismiss()
                 //binding.sendOTP.showNormalButton()
 
                 binding.numberLayout.visibility = GONE
@@ -99,12 +117,42 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 //binding.sendOTP.showNormalButton()
                 if (task.isSuccessful) {
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+
+                    checkUserExist(binding.userNumber.text.toString())
+
+//                    startActivity(Intent(this, MainActivity::class.java))
+//                    finish()
                 } else {
+
+                    dialog.dismiss()
                     Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun checkUserExist(number: String) {
+
+        FirebaseDatabase.getInstance().getReference("users").child(number)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    dialog.dismiss()
+                    Toast.makeText(this@LoginActivity, p0.message, Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    if(p0.exists()) {
+                        dialog.dismiss()
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                        finish()
+                    } else {
+                        startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+                        finish()
+                    }
+                }
+
+            })
+
     }
 
 }
