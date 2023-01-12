07479e2603e54a369424a0cef91866f69757165d
Mario Medina <kumihontou@gmail.com>
2023-01-12 12:24:12 +0100

    Third commit

--- app/src/main/java/com/mariomedina/gamehub/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/mariomedina/gamehub/activity/SplashScreenActivity.kt
@@ -23,6 +23,7 @@ class SplashScreenActivity : AppCompatActivity() {
                 startActivity(Intent(this, LoginActivity::class.java))
             else
                 startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }, 2000)
     }
 }
--- app/src/main/java/com/mariomedina/gamehub/auth/LoginActivity.kt
+++ app/src/main/java/com/mariomedina/gamehub/auth/LoginActivity.kt
@@ -1,12 +1,110 @@
 package com.mariomedina.gamehub.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.mariomedina.gamehub.R
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.mariomedina.gamehub.MainActivity
+import com.mariomedina.gamehub.databinding.ActivityLoginBinding
+import java.util.concurrent.TimeUnit
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+
+    val auth = FirebaseAuth.getInstance()
+    private var verificationId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendOTP.setOnClickListener{
+            if (binding.userNumber.text!!.isEmpty()) {
+                binding.userNumber.error = "Por favor introduce un número"
+            } else {
+                verifyOTP(binding.userNumber.text.toString())
+            }
+        }
+
+        binding.verifyOTP.setOnClickListener{
+            if (binding.userOTP.text!!.isEmpty()) {
+                binding.userOTP.error = "Por favor introduce el Código de Seguridad"
+            } else {
+                sendOTP(binding.userOTP.text.toString())
+            }
+        }
     }
+
+    private fun verifyOTP(otp: String) {
+
+        //binding.sendOTP.showLoadingButton()
+
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
+
+        signInWithPhoneAuthCredential(credential)
+
+    }
+
+    private fun sendOTP(number: String) {
+
+        //binding.sendOTP.showLoadingButton()
+
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+                //binding.sendOTP.showNormalButton()
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                this@LoginActivity.verificationId = verificationId
+
+                //binding.sendOTP.showNormalButton()
+
+                binding.numberLayout.visibility = GONE
+                binding.OTPLayout.visibility = VISIBLE
+            }
+        }
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber("+34$number")       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                //binding.sendOTP.showNormalButton()
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
