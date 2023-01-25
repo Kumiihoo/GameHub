7dd3be2cda080472814657a5beb770b5ba6c8099
Mario Medina <kumihontou@gmail.com>
2023-01-25 15:46:39 +0100

    V. 1.0.0

--- app/src/main/java/com/mariomedina/gamehub/model/UserModel.kt
+++ app/src/main/java/com/mariomedina/gamehub/model/UserModel.kt
@@ -5,10 +5,5 @@ data class UserModel(
     val name :String? = "",
     val email :String? = "",
     val city :String? = "",
-    val gender :String? = "",
-    val game :String? = "",
-    val star :String? = "",
     val image :String? = "",
-    val age :String? = "",
-    val status :String? = ""
 )
